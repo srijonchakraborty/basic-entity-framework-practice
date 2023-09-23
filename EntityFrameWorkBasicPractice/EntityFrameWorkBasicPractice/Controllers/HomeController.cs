184e7b1e2c3a9aa1749dc429bdc41ea7f8e329ab
srijonchakraborty <srijonchak@live.com>
2023-09-23 19:23:19 +0600

    First Commit 1

---
+++ EntityFrameWorkBasicPractice/EntityFrameWorkBasicPractice/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace EntityFrameWorkBasicPractice.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+        [HttpPost("checkHome")]
+        public async Task<ActionResult<dynamic>> CheckHome([FromBody] dynamic checkHomeRequest)
+        {
+            if (ModelState.IsValid)
+            {
+                return Ok("Done");
+            }
+            return BadRequest(ModelState);
+        }
+    }
+}
